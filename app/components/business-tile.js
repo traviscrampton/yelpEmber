bc3b9c3cb0f848d69b0e57fb77104acd07316f85
travis crampton & josue valverde <student@epicodus.com>
2015-10-01 10:08:05 -0700

    create list for cities and have them drop down to the cities that are associated

--- app/components/business-tile.js
+++ app/components/business-tile.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showDaBidnessForm : false,
+
+  actions : {
+    showDaBidness() {
+      this.set('showDaBidnessForm', true);
+    },
+
+    hideDaBidness() {
+      this.set('showDaBidnessForm', false);
+    }
+  }
 });
